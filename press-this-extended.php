72c12b3c2dc15cd74ae66dd2a19ca614872f36a9
Brandon Kraft <public@brandonkraft.com>
2015-04-17 22:55:41 -0500

    First pass at pointer.

--- press-this-extended.php
+++ press-this-extended.php
@@ -501,3 +501,74 @@ class Press_This_Extended {
 
 // Giddy up.
 $Press_This_Extended = new Press_This_Extended;
+
+add_action( 'admin_enqueue_scripts', 'pte_admin_pointers_header' );
+
+	function pte_admin_pointers_header() {
+		if ( pte_admin_pointers_check() ) {
+			add_action( 'admin_print_footer_scripts', 'pte_admin_pointers_footer' );
+
+			wp_enqueue_script( 'wp-pointer' );
+			wp_enqueue_style( 'wp-pointer' );
+		}
+	}
+
+	function pte_admin_pointers_check() {
+		$admin_pointers = pte_admin_pointers();
+		foreach ( $admin_pointers as $pointer => $array ) {
+			if ( $array['active'] )
+				return true;
+		}
+	}
+
+	function pte_admin_pointers_footer() {
+		$admin_pointers = pte_admin_pointers();
+		?>
+		<script type="text/javascript">
+		/* <![CDATA[ */
+		( function($) {
+			<?php
+			foreach ( $admin_pointers as $pointer => $array ) {
+				if ( $array['active'] ) {
+					?>
+					$( '<?php echo $array['anchor_id']; ?>' ).pointer( {
+						content: '<?php echo $array['content']; ?>',
+						position: {
+							edge: '<?php echo $array['edge']; ?>',
+							align: '<?php echo $array['align']; ?>'
+						},
+						close: function() {
+							$.post( ajaxurl, {
+								pointer: '<?php echo $pointer; ?>',
+								action: 'dismiss-wp-pointer'
+							} );
+						}
+					} ).pointer( 'open' );
+					<?php
+				}
+			}
+			?>
+		} )(jQuery);
+		/* ]]> */
+		</script>
+		<?php
+	}
+
+	function pte_admin_pointers() {
+		$dismissed = explode( ',', (string) get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
+		$version = '1_0'; // replace all periods in 1.0 with an underscore
+		$prefix = 'pte_admin_pointers' . $version . '_';
+
+		$new_pointer_content = '<h3>' . __( 'Setup Press This Extended' ) . '</h3>';
+		$new_pointer_content .= '<p>' . __( 'Modify the default settings to Press This in the Writing section.' ) . '</p>';
+
+		return array(
+			$prefix . 'new_items' => array(
+				'content' => $new_pointer_content,
+				'anchor_id' => '#menu-settings',
+				'edge' => 'left',
+				'align' => 'center',
+				'active' => ( ! in_array( $prefix . 'new_items', $dismissed ) )
+				),
+			);
+	}
