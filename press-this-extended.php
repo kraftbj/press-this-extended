kraftbj/press-this-extended

commit 52630b33c20fef4218634b447c601bb0f718fded

Adds contextual help.
Fixes #9

String Freeze. Closes #10

press-this-extended.php
@@ -443,7 +443,11 @@ class Press_This_Extended {
 		get_current_screen()->add_help_tab( array(
 			'id'      => 'options-press-this-extended',
 			'title'   => __('Press This'),
-			'content' => '<p>' . __( 'Filler text. These options allow you to customize the Press This bookmarklet to do some cool stuff.' ) . '</p>',
+			'content' => '<p>' . __( 'These options allow you to customize the Press This bookmarklet to do some cool stuff.' ) . '</p>' .
+			           . '<p>' . __( 'Content Grabbing refers to Press This suggesting media and text for you to add in your pressed post. By default, Press This will suggest a blockfrom from the page\'s description unless it appears to be autogenerated.', 'press-this-extended' ) . '</p>'
+			           . '<p>' . __( 'Blockquote and Citation Formatting allows you to adjust the default HTML template used when pressing a post.', 'press-this-extended' ) . '</p>'
+			           . '<p>' . __( 'Redirection, Upon Publishing..., and Upon Saving A Draft... adjusts what happens when you save your content from Press This.', 'press-this-extended' ) . '</p>'
+			           . '<p>' . __( 'Text Editor adds the HTML option found in the common editor. Some features of Press This, such as inserting the suggested media or the Save Draft text changing may not work as expected when using the text editor.', 'press-this-extended' ) . '</p>',
 			)
 		);
 	}

commit df2bf98646695f3f25bf268544478d96e6ef3c6c

Fixes syntax error introduced in 52630b33c20fef4218634b447c601bb0f718fded

press-this-extended.php
@@ -444,10 +444,10 @@ class Press_This_Extended {
 			'id'      => 'options-press-this-extended',
 			'title'   => __('Press This'),
 			'content' => '<p>' . __( 'These options allow you to customize the Press This bookmarklet to do some cool stuff.' ) . '</p>' .
-			           . '<p>' . __( 'Content Grabbing refers to Press This suggesting media and text for you to add in your pressed post. By default, Press This will suggest a blockfrom from the page\'s description unless it appears to be autogenerated.', 'press-this-extended' ) . '</p>'
-			           . '<p>' . __( 'Blockquote and Citation Formatting allows you to adjust the default HTML template used when pressing a post.', 'press-this-extended' ) . '</p>'
-			           . '<p>' . __( 'Redirection, Upon Publishing..., and Upon Saving A Draft... adjusts what happens when you save your content from Press This.', 'press-this-extended' ) . '</p>'
-			           . '<p>' . __( 'Text Editor adds the HTML option found in the common editor. Some features of Press This, such as inserting the suggested media or the Save Draft text changing may not work as expected when using the text editor.', 'press-this-extended' ) . '</p>',
+			             '<p>' . __( 'Content Grabbing refers to Press This suggesting media and text for you to add in your pressed post. By default, Press This will suggest a blockfrom from the page\'s description unless it appears to be autogenerated.', 'press-this-extended' ) . '</p>' .
+			             '<p>' . __( 'Blockquote and Citation Formatting allows you to adjust the default HTML template used when pressing a post.', 'press-this-extended' ) . '</p>' .
+			             '<p>' . __( 'Redirection, Upon Publishing..., and Upon Saving A Draft... adjusts what happens when you save your content from Press This.', 'press-this-extended' ) . '</p>' .
+			             '<p>' . __( 'Text Editor adds the HTML option found in the common editor. Some features of Press This, such as inserting the suggested media or the Save Draft text changing may not work as expected when using the text editor.', 'press-this-extended' ) . '</p>',
 			)
 		);
 	}